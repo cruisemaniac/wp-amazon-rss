5f838c04b2a271d872c601bf2adefc7cb115cf25
Ashwin Murali <mail@cruisemaniac.com>
2013-06-06 00:52:43 +0530

    Added credits where due.

--- wp-amazon-rss.php
+++ wp-amazon-rss.php
@@ -9,7 +9,9 @@ Version: 0.1
 Author URI: http://imashw.in
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation; 
+the Free Software Foundation;
+
+Credit: https://github.com/kpgraham/Wordpress-RSS2POST
 */
 
 
